f1a68b02bc75e1bb1a4119e5a1e40733507f91e5
Ivan <oxyd.apps@gmail.com>
2019-11-08 08:23:53 +0200

    add test login page

---
+++ pages/login_page.py
@@ -0,0 +1,17 @@
+from .base_page import BasePage
+from .locators import MainPageLocators
+
+class LoginPage(BasePage):
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
+
+    def should_be_login_url(self):
+        assert "login" in self.browser.current_url, "it is not login url"
+
+    def should_be_login_form(self):
+        assert self.browser.find_element(*LoginPageLocators.LOGIN_FORM), "not login form on page"
+
+    def should_be_register_form(self):
+        assert self.browser.find_element(*LoginPageLocators.REGISTER_FORM), "not register form on page"
