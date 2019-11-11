34bef08783ecf5e31d56b5827222f0e9e41e6aa8
Ivan <oxyd.apps@gmail.com>
2019-11-11 12:44:19 +0200

    change test product page with params offers and mark fail param as xfail

--- test_product_page.py
+++ test_product_page.py
@@ -1,10 +1,17 @@
 from pages.product_page import ProductPage
+import pytest
 
-link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
-def test_guest_can_add_product_to_basket(browser):
+@pytest.mark.parametrize('promo_offer', ["0","1", "3", "4", "5", "6",
+                                  pytest.param("7", marks=pytest.mark.xfail),
+                                  "8","9"])
+                                  
+def test_guest_can_add_product_to_basket(browser, promo_offer):
+    link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{promo_offer}"
     page = ProductPage(browser, link)
     page.open()
     page.click_to_add_to_basket_button()
     page.solve_quiz_and_get_code()
     page.should_be_product_message_to_basket()
     page.should_be_price_in_basket()
+
+
