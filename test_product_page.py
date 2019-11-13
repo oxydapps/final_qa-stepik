91a759727f2269ffcc4faeeec68d485ada6f2835
Ivan <oxyd.apps@gmail.com>
2019-11-13 11:45:27 +0200

    fix merge git

--- test_product_page.py
+++ test_product_page.py
@@ -44,7 +44,6 @@ def test_guest_should_see_login_link_on_product_page(browser):
     page = ProductPage(browser, link)
     page.open()
     page.should_be_login_link()
-<<<<<<< HEAD
 
 @pytest.mark.need_review
 def test_guest_can_go_to_login_page_from_product_page(browser):
@@ -53,18 +52,6 @@ def test_guest_can_go_to_login_page_from_product_page(browser):
     page.open()
     page.go_to_login_page()
 
-@pytest.mark.need_review
-=======
-    
-@pytest.mark.need_review
-def test_guest_can_go_to_login_page_from_product_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
-    page = ProductPage(browser, link)  
-    page.open()
-    page.go_to_login_page()
-
-@pytest.mark.need_review
->>>>>>> a927b2a9d88df10f0c62f5c62f7eb5462b0632c2
 def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
